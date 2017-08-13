c4415bac5cf0f7968530d03549e7414ccc08f21d
martinirani <martin.irani@ug.uchile.cl>
2017-08-12 21:49:48 -0400

    Corrections on the perceptron function

--- redes/Perceptron.py
+++ redes/Perceptron.py
@@ -1,4 +1,5 @@
-from numpy import array, dot
+import numpy
+
 
 # [CC501] Ex.1. Perceptron
 # Tarea Lunes
@@ -6,35 +7,29 @@ from numpy import array, dot
 
 
 class Perceptron:
-
-
-    def __init__(self, x, bias, weight):
+    def __init__(self, x, weight, bias):
         self.x = x
-        self.config(bias,weight)
+        self.weight = weight
+        self.bias = bias
 
     def output(self):
-        if (dot(self.x, self.weight) + self.bias) > 0:
+        if (numpy.dot(self.x, self.weight) + self.bias) > 0:
             return 1
         else:
             return 0
 
-    def set_bias(self, bias):
-        self.bias = bias
-
-    def set_weight(self, weight):
-        self.weight = weight
 
 class PerceptronNand(Perceptron):
     def __init__(self, x):
         self.x = x
-        self.bias= len(x) * 1
-        self.weight= len(x) * [-1]
+        self.bias = len(x) * 1
+        self.weight = len(x) * [-1]
 
 
 class PerceptronAnd(Perceptron):
     def __init__(self, x):
         self.x = x
-        self.bias= len(x) * -1 + 1
+        self.bias = len(x) * -1 + 1
         self.weight = len(x) * [1]
 
 
@@ -43,4 +38,3 @@ class PerceptronOr(object, Perceptron):
         self.x = x
         self.bias = 0
         self.weight = len(x) * [1]
-
