a6629249ffd2efdc1866b2eeb1d35f5acf1f9ae7
martinirani <martin.irani@ug.uchile.cl>
2017-08-17 00:03:58 -0300

    tarea 2

--- redes/SigmoidNeuron.py
+++ redes/SigmoidNeuron.py
@@ -1,59 +1,92 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+n_points = 100
+
 
 class SigmoidNeuron:
 
-    def __init__(self, x, bias, weight):
+    def __init__(self, x, bias, weight, threshold):
 
         self.x = x
         self.bias = bias
         self.weight = weight
+        self.threshold = threshold
+
 
     def zvalue(self):
-        return np.dot(self.x, self.weight) + self.bias
+        value = np.dot(self.x, self.weight) + self.bias
+        print value
+        return value
+
+    def sigma(self):
+        sigma = 1/(1 + np.exp(-(self.zvalue())))
+        return sigma
 
     def output(self):
-        return 1/(1 + np.exp(SigmoidNeuron(self.x, self.bias, self.weight).zvalue()))
+        if self.sigma > self.threshold:
+            return 1
+        else:
+            return 0
 
 
 class LearningSigmoid(object, SigmoidNeuron):
-    def __init__(self, x, y, bias, learningrate, train_num):
+    def __init__(self, x, y, bias, learningrate, train_num, threshold):
         self.x = x
         self.y = y
         self.bias = bias
-        self.weight = np.random.rand(1, 1)     # weights are picked randomly to start
+        self.weight = np.random.rand(2, 1)  # weights are picked randomly to start
         self.learningrate = learningrate
         self.train_num = train_num
+        self.xy = np.concatenate((x, y), axis=1)
+        self.guess = np.zeros(np.shape(y))
+        self.threshold = threshold
 
     def train(self):
 
-        for i in xrange(0, self.train_num):
+        for ii in xrange(0, self.train_num):
+            for i in xrange(0, np.shape(self.x)[0]):
 
-            guess = SigmoidNeuron.output(self.x[i])
-            desired = self.y[i]
-            error = desired - guess
+                self.guess[i] = SigmoidNeuron(self.xy[i, 0:2], self.weight, self.bias, self.threshold).output()
 
-            if error > 0:
-                self.weight = self.weight + self.learningrate * self.x[i]
-            elif error < 0:
-                self.weight = self.weight - self.learningrate * self.x[i]
-            else:
-                self.weight = self.weight
+                desired = self.xy[i, 2:3]
+                sign = desired-self.guess[i]
+                self.weight = self.weight + sign*np.transpose([self.xy[i, 0:2]])*self.learningrate
+
+                #print self.weight
+
+    def color(self, c):
+        if c ==0:
+            return 'red'
+        else:
+            return 'blue'
 
     def draw(self):
-        plt.scatter(self.x, self.y)
-        plt.plot(self.x, SigmoidNeuron(self.x, self.bias, self.weight).output())
+        for i in range(0,n_points):
+            plt.scatter(self.x[i,0:1], self.x[i,1:2], c='blue', marker='o')
+        for i in range(n_points, 2*n_points):
+            plt.scatter(self.x[i, 0:1], self.x[i,1:2], c='red', marker='o')
+
+        plt.plot(self.x[:,0:1], self.x[:,0:1]*self.weight[0] + self.bias)
         plt.show()
 
-x = np.random.rand(50, 1)
-y = np.random.rand(50, 1)
-train_num = 50
+x_population1 = np.random.normal(np.array([20, 20]), 50.0, np.array([n_points, 2]))
+x_pop1_label = np.zeros((n_points, 1)) + 1
+
+x_population2 = np.random.normal(np.array([0, 0]), 50.0, np.array([n_points, 2]))
+x_pop2_label = np.zeros((n_points, 1))
+
+x = np.concatenate((x_population1, x_population2), axis=0)
+y = np.concatenate((x_pop1_label, x_pop2_label), axis=0)
+
+train_num = 500
 b = 1
-learningrate = 0.01
+learningrate = 0.001
+threshold = 0.5
 
-LearningSigmoid(x, y, b, learningrate, train_num).draw()
-
+percyval = LearningSigmoid(x, y, b, learningrate, train_num, threshold)
+percyval.train()
+percyval.draw()
 
 
 
